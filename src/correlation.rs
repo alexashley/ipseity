b42ab5edceef7b7eb38434c72525ed12befb0d0a
Alex Ashley <alexashley15@gmail.com>
2019-04-17 19:13:53 -0500

    Create correlation id for each request

---
+++ src/correlation.rs
@@ -0,0 +1,22 @@
+extern crate iron;
+extern crate uuid;
+
+use iron::prelude::*;
+use iron::{BeforeMiddleware, typemap};
+use uuid::Uuid;
+
+pub struct Correlation;
+
+impl typemap::Key for Correlation { type Value = Uuid; }
+
+impl BeforeMiddleware for Correlation {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        let correlation_id = Uuid::new_v4();
+
+        req.extensions.insert::<Correlation>(correlation_id);
+
+        println!("{}", correlation_id);
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 extern crate iron;
 extern crate router;
+extern crate uuid;
 
 use iron::prelude::*;
 use iron::status;
 use router::Router;
+
+//use uuid::Uuid;
+
+mod correlation;
 
 fn well_known(req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "{}")))
@@ -13,5 +18,9 @@ fn main() {
     let mut router = Router::new();
     router.get("/.well-known/openid-configuration", well_known, "openid-configuration");
 
-    Iron::new(router).http("localhost:8080").unwrap();
+    let mut chain = Chain::new(router);
+
+    chain.link_before(correlation::Correlation);
+
+    Iron::new(chain).http("localhost:8080").unwrap();
 }
